a84e76d2e4e859b084f1f4f7e85c1348b642be7c
hamedsoum <hamedsoum4023@gmail.com>
2022-07-15 00:39:49 +0000

    add new todo

--- Todo_App/index.py
+++ Todo_App/index.py
@@ -20,12 +20,26 @@ def logIn(userName,password):
     else :
         print('User logged failed')
 
+def actionsToMake():
+    action = input('what do you want to do? 1 for list all to-dos or 2 for add a new to-do ')
+    if int(action) == 1 :
+        print(todoList)
+    elif int(action) == 2 :
+      expense = input('expense :') 
+      expenseAmount = input('amount :' ) 
+      todo_dist = {
+          'expense' : expense,
+          'expenseAmount' : expenseAmount
+      }
+      todoList.append(todo_dist)
+      print(todoList)
 
 print('welcome to the todo App! ')
 
 print('let\'s start with a creation of you account ')
 
-firstName = input('what is your firstName ?')
+firstName = input('what is your firstName ?  ' )
+
 lastName = input('lastName :')
 userName = input('userName :')
 age = input('age :')
@@ -40,6 +54,7 @@ informationCheck = input('confirm information? (1 for yes and 0 for no)')
 
 if int(informationCheck) == 1 :
    logIn(userName,password)
+   actionsToMake()
 else :
     print('information incorect')
 
